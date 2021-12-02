a2b081cea3b7117082d9510fcdba41c8b000817e
nyamadip <nyamadip@stanbic.com.gh>
2021-12-02 19:54:57 +0000

    code clean up

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,6 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-    
-    // const [name, setName] = useState('mario')
-
-
-
-   
-    // useEffect(() => {
-    //     console.log('use effect ran');
-    //     // console.log(blogs)
-    //     console.log(name)
-    // }, [name])
 
     useEffect(() => {
             fetch('http://localhost:8000/blogs')
@@ -30,8 +19,7 @@ const Home = () => {
         <div className="home">
             {
                 blogs && <BlogList blogs={blogs}  title="All Blogs!" /> }
-            {/* <button onClick={() => setName('luigi')} >Change name</button>
-            <p>{name}</p> */}
+           
         </div>
      );
 }
