2d7ab645aec1ebc783a9b86df8bf78e957f23e55
nyamadip <nyamadip@stanbic.com.gh>
2021-12-02 01:15:25 +0000

    handling click events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 const Home = () => {
+
+    const handleClick = () =>{
+        console.log('hello, ninjas')
+    }
+
+    const handleClickAgain = (name) =>{
+        console.log('hello', name)
+    }
+
     return ( 
         <div className="home">
             <h2>
                 Home page
             </h2>
+            <button onClick={handleClick} >Click me</button>
+            <button onClick={() => handleClickAgain('yoshi')}>Click me again</button>
         </div>
      );
 }
