160cdcfe50f92a02ca94ac6e0781a74ffd018bf1
nyamadip <nyamadip@stanbic.com.gh>
2021-12-02 19:31:45 +0000

    setting up useEffect to trigger on certain conditions

--- src/Home.js
+++ src/Home.js
@@ -7,19 +7,26 @@ const Home = () => {
         {title: 'Welcome party', body: 'lorem ipsum...', author: 'yoshi', id:2},
         {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id:3}
     ]);
- 
+    
+    const [name, setName] = useState('mario')
+
+
     const handleDelete = (id)=>{
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
    
     useEffect(() => {
-        console.log('use effect run');
-    })
+        console.log('use effect ran');
+        // console.log(blogs)
+        console.log(name)
+    }, [name])
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs}  title="All Blogs!" handleDelete={handleDelete}/> 
+            <button onClick={() => setName('luigi')} >Change name</button>
+            <p>{name}</p>
         </div>
      );
 }
