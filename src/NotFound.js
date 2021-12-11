d949b600a4c71b9ec87e852995d0fee3c95eb6af
nyamadip <nyamadip@stanbic.com.gh>
2021-12-11 15:44:41 +0000

    ...

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+ const NotFound = ()=>{
+     return (
+         <div className="not-found">
+             <h2>
+                 Sorry
+             </h2>
+             <p>Page cannot be found</p>
+             <Link to="/">Back to the homepage...</Link>
+         </div>
+     )
+ }
